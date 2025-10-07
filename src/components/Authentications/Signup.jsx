7d178b746921ba5c4865ffb3da7e7e893211e996
Muhammad Furqan <furqanf64123@gmail.com>
2025-10-07 13:12:49 +0500

    yes

--- src/components/Authentications/Signup.jsx
+++ src/components/Authentications/Signup.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AuthContext } from "@/AppContext/AppContext";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
